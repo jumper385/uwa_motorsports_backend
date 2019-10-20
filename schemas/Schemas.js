8fee9052e778bafe641c4ffebb5838e3c316ee3b
Henry Chen <henrychen385@gmail.com>
2019-10-20 23:17:09 +0800

    schema comments lol

--- schemas/Schemas.js
+++ schemas/Schemas.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose')
 
+// @desc different schemas
 const UserSchema = new mongoose.Schema({
     username:String,
     password:String,
     email:String,
 })
 
+// @desc schema output
 module.exports = {
     User:UserSchema
 }
