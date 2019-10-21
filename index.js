9b2aac8bdf92b66550d2e2ab31eeabc3591b4fb1
Henry Chen <henrychen385@gmail.com>
2019-10-21 08:15:58 +0800

    added basic multer + gridfs support

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ mongoose.connect(
     {useNewUrlParser:true, useUnifiedTopology:true}
 )
 
+const storage = GridFsStorage({
+    url:'mongodb://localhost/uwa_motorsports',
+})
+
+const upload = multer({ storage:storage, useUnifiedTopology:true })
+
 // @desc mongoose schemas
 const Schemas = require('./schemas/Schemas')
 const User = mongoose.model('User', Schemas.User)
@@ -24,6 +30,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 // @desc http request handlers
 app.get('/', (req,res) => {
     res.json({message:'hello world'})
+})
+
+app.post('/', upload.single('photos'), (req,res,next) => {
+    
+    const mongoPayload = {
+        file:req.file,
+        body:req.body
+    }
+    
+    res.json(mongoPayload)
 })
 
 // @desc app listeners
